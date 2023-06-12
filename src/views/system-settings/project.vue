d3368a6fb63201be5f55afc4a434193d4eaafd8b
fjqken <469965307@qq.com>
2023-06-12 17:27:45 +0800

    随便搞搞

--- src/views/system-settings/project.vue
+++ src/views/system-settings/project.vue
@@ -12,18 +12,19 @@
         </div>
       </div>
       <el-table
-        :data="tableData"
+        :data="tableData.slice((pageInfo.currentPage-1)*pageInfo.pageSize,pageInfo.currentPage*pageInfo.pageSize)"
         stripe
         style="width: 100%"
+        :height="tableH"
       >
         <el-table-column
-          prop="date"
-          label="日期"
+          prop="project_name"
+          label="项目名称"
           width="180"
         />
         <el-table-column
-          prop="name"
-          label="姓名"
+          prop="create_time"
+          label="创建日期"
           width="180"
         />
         <el-table-column
@@ -31,15 +32,15 @@
           label="地址"
         />
       </el-table>
-      <div class="pagination">
-        <el-pagination
-          v-if="total > 0"
-          layout="prev, pager, next"
-          :total="total"
-          :page-size="pageSize"
-          @current-change="handleCurrentChange"
-        />
-      </div>
+      <el-pagination
+        :current-page="pageInfo.currentPage"
+        :page-sizes="[2, 5, 10, 20]"
+        :page-size="pageInfo.pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="pageInfo.pageTotal"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
     </div>
   </div>
 </template>
@@ -49,53 +50,59 @@ export default {
   name: 'Project',
   data() {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }], // 表格数据
-      total: 0, // 总记录数
-      pageSize: 10, // 每页显示记录数
-      currentPage: 1 // 当前页码
+      tableH: 600,
+      tableData: [
+        {
+          create_time: '2016-05-01',
+          project_name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          create_time: '2016-05-02',
+          project_name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          create_time: '2016-05-03',
+          project_name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        },
+        {
+          create_time: '2016-05-04',
+          project_name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          create_time: '2016-05-05',
+          project_name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          create_time: '2016-05-06',
+          project_name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }
+      ], // 表格数据
+      pageInfo: {
+        currentPage: 1,
+        pageSize: 10,
+        pageTotal: 20
+      }
     }
   },
-  mounted() {
-    // 加载默认第一页数据
-    // this.loadData(1)
+  computed: {},
+  created() {
+    this.pageInfo.pageTotal = this.tableData.length
   },
+  mounted() {
+  },
+
   methods: {
-    loadData(page) {
-      // 模拟异步加载数据
-      setTimeout(() => {
-        const start = (page - 1) * this.pageSize
-        const end = start + this.pageSize
-        const data = []
-        for (let i = start; i < end; i++) {
-          data.push({
-            name: `用户${i + 1}`,
-            age: Math.floor(Math.random() * 50) + 20,
-            gender: Math.random() > 0.5 ? '男' : '女'
-          })
-        }
-        // this.tableData = data
-        this.total = 100 // 总记录数
-        this.currentPage = page // 当前页码
-      }, 500)
+    handleSizeChange(val) {
+      this.pageInfo.pageSize = val
     },
-    handleCurrentChange(page) {
-      this.loadData(page)
+    handleCurrentChange(val) {
+      this.pageInfo.currentPage = val
     }
   }
 }
