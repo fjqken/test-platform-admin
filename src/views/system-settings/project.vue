da140e86a54814f62e987b5b26c2af26922281f3
fjqken <469965307@qq.com>
2023-07-04 17:26:59 +0800

    增加新增项目窗口

--- src/views/system-settings/project.vue
+++ src/views/system-settings/project.vue
@@ -8,9 +8,23 @@
           </el-input>
         </div>
         <div>
-          <el-button type="primary">新增项目</el-button>
+          <el-button type="primary" @click="dialogFormVisible = true">新增项目</el-button>
         </div>
       </div>
+      <el-dialog title="新增项目" :visible.sync="dialogFormVisible">
+        <el-form ref="add_form" :model="add_form" :rules="rules">
+          <el-form-item label="项目名称" :label-width="formLabelWidth" prop="project_name">
+            <el-input v-model="add_form.project_name" autocomplete="off" />
+          </el-form-item>
+          <el-form-item label="项目地址" :label-width="formLabelWidth" prop="address">
+            <el-input v-model="add_form.address" autocomplete="off" />
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="submitForm('add_form')">确 定</el-button>
+        </div>
+      </el-dialog>
       <el-table
         :data="tableData.slice((pageInfo.currentPage-1)*pageInfo.pageSize,pageInfo.currentPage*pageInfo.pageSize)"
         stripe
@@ -52,9 +66,31 @@ export default {
   name: 'Project',
   data() {
     return {
+      dialogFormVisible: false,
+      add_form: {
+        project_name: '',
+        address: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      rules: {
+        project_name: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        address: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ]
+      },
+      formLabelWidth: '120px',
+      input3: null,
       tableH: 600,
-      tableData: [
-      ], // 表格数据
+      tableData: [], // 表格数据
       pageInfo: {
         currentPage: 1,
         pageSize: 10,
@@ -64,10 +100,7 @@ export default {
   },
   computed: {},
   created() {
-    project_list({
-      create_user_id: 1,
-      project_name: '汕尾 '
-    }).then(response => {
+    project_list({}).then(response => {
       console.log(response.data)
       this.tableData = response.data
     })
@@ -82,6 +115,17 @@ export default {
     },
     handleCurrentChange(val) {
       this.pageInfo.currentPage = val
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+          this.dialogFormVisible = false
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
     }
   }
 }
